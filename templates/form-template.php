a977cd0c777a4e31d13cf159209b07b258a55971
MahirEO <83029372+MahirEO@users.noreply.github.com>
2025-10-17 05:43:35 +0600

    Create form-template.php for campus ambassador forms
    Added a new template for campus ambassador forms.

---
+++ templates/form-template.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Form Template
+ * 
+ * Template for rendering campus ambassador forms
+ */
+
+// Prevent direct access
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+// Form template stub
+?>
