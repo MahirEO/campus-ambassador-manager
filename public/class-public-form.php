3fc7bd5b7ad49a7ba0bdc89659848c9aeedb6f6b
MahirEO <83029372+MahirEO@users.noreply.github.com>
2025-10-17 05:36:27 +0600

    Add class-public-form.php

---
+++ public/class-public-form.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Public Form Handler Class
+ *
+ * @package Campus_Ambassador_Manager
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class Campus_Ambassador_Public_Form {
+
+	public function __construct() {
+		// Constructor
+	}
+
+	public function render_form() {
+		// Render form logic
+	}
+
+	public function process_submission() {
+		// Process form submission
+	}
+}
